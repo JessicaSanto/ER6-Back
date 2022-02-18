f52ca05ba7229cfc0b237516ad4065695eac196f
JessicaSanto <jessicaesanto@outlook.com>
2022-02-18 15:41:39 -0300

    er5

---
+++ Pessoa.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EncontroRemoto
+{
+    public abstract class Pessoa
+    {
+        public string? nome { get; set; }
+        public Endereco? endereco { get; set; }        
+        public abstract void PagarImposto(float salario);
+    }
+}
---
+++ PessoaJuridica.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace EncontroRemoto
+{
+    public class PessoaJuridica : Pessoa
+    {
+        public string? cnpj { get; set; }
+
+        public string? razaoSocial { get; set; }
+
+        public override void PagarImposto(float salario)
+        {
+        }
+
+    }
+}
